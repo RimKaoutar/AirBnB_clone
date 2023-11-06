d4ffb9eff4bef0a0d65071f79d18e1b52f633b84
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-11-06 16:43:24 +0100

    done task5: Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,20 @@
 """the main entry to the BaseModel for the ABnB console"""
 import uuid
 import datetime
+from models import storage
 
 
 class BaseModel:
     """the BaseModel class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init the BaseModel obj"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            pass
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """printable formatted stirng of the objj"""
@@ -20,6 +25,7 @@ class BaseModel:
     def save(self):
         """update the updated_at attr"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """return a dictionnary of the current class attributes
