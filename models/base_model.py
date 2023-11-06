ee744957f32812c2c893c4539e3384f85d2d481d
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-06 16:55:31 +0100

    updated the constructor for the desserialisation process

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,22 @@ import datetime
 
 class BaseModel:
     """the BaseModel class"""
-    def __init__(self):
-        """init the BaseModel obj"""
+    def __init__(self, *av, **kav):
+        """init the BaseModel obj
+        Args:
+            av (list): the list of args , --not_used--
+            kav (dict): the dictionnary of args
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kav:
+            for key, value in kav.items():
+                if (key == "__class__"):
+                    continue
+                elif (key == "created_at" or key == "updated_at"):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
 
     def __str__(self):
         """printable formatted stirng of the objj"""
