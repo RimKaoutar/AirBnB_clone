a7b68d3c82009c5080f3b746fc49399bd8d92703
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-06 19:34:15 +0100

    updated console.py

--- console.py
+++ console.py
@@ -2,11 +2,21 @@
 """contains the entry point of the command interpreter"""
 
 import cmd
+import models.engine.file_storage
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand(cmd.Cmd)"""
 
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -16,6 +26,20 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty line shouldn't execute anything"""
         pass
+    
+    def do_create(self, args):
+        """create an instance of a class type
+        Args:
+            args (obj): an object type, if no match in the
+            classes list, output error
+        """
+        arglist = args.split()
+        if len(arglist) == 0:
+            print("** class name missing **")
+        elif arglist[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arglist[0]().id))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
