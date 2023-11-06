de14f0922d88a03a2e4e52d87860e6a64c0d9ee4
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-11-06 18:02:58 +0100

    updated task5

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,7 @@ class BaseModel:
             av (list): the list of args , --not_used--
             kav (dict): the dictionnary of args
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
         if kav:
             for key, value in kav.items():
                 if (key == "__class__"):
@@ -23,6 +21,11 @@ class BaseModel:
                 elif (key == "created_at" or key == "updated_at"):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """printable formatted stirng of the objj"""
