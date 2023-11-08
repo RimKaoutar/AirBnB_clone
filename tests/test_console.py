c66eaabf4e015b20d68895d25bd5177c38322295
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-11-09 00:57:25 +0100

    test for quit

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,13 +3,26 @@
 Unit tests for console.py
 """
 import unittest
+import os
+from io import StringIO
+from unittest.mock import patch
+from console import HBNBCommand
 
 
-class TestHBNBCommand(unittest.TestCase):
-    """Tests for the HBNBCommand class"""
+class TestHBNBCommandQuit(unittest.TestCase):
+    """Unittests for testing quiting the HBNB command interpreter."""
 
-    pass
+    def test_quit_exits(self) -> None:
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF_exits(self) -> None:
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
 
 
+
+
+        
 if __name__ == "__main__":
     unittest.main()
