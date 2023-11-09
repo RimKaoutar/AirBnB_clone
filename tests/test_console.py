42b49f8fb0d0d58f6816e2e10e2fae6f41882cae
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-11-09 01:04:14 +0100

    test for create feature

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,13 +5,15 @@ Unit tests for console.py
 Unittest classes:
     TestHBNBCommandQuit
     TestHBNBCommandEmptyLine
+    TestHBNBCommandCreate
 """
 import unittest
 import os
 from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
-
+from models import storage
+from models.engine.file_storage import FileStorage
 
 class TestHBNBCommandQuit(unittest.TestCase):
     """Unittests for testing quiting the HBNB command interpreter."""
@@ -32,6 +34,27 @@ class TestHBNBCommandEmptyLine(unittest.TestCase):
             self.assertEqual("", output.getvalue().strip())
 
 
+class TestHBNBCommandCreate(unittest.TestCase):
+    """Unittests for testing create in the HBNB command interpreter."""
+
+    @classmethod
+    def setUp(self) -> None:
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        FileStorage.__objects = {}
+
+    @classmethod
+    def tearDown(self) -> None:
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
         
 if __name__ == "__main__":
     unittest.main()
