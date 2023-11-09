b3f93cda107c1f572fc35c5ecbd1b395d661ca1c
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-09 15:17:15 +0100

    testing

--- console.py
+++ console.py
@@ -74,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
         if arg is None or arg == "":
             print("** class name missing **")
         else:
-            args = arg.split()
+            args = [i.strip(",:.\'\"{[]}") for i in arg.split()]
             if args[0] not in storage.classes():
                 print("** class doesn't exist **")
             elif len(args) < 2:
